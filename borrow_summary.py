1ab5fa3868a16232be7d3d25e4f22952ffbfcd1b
mitsutoshi <mitsu3204@gmail.com>
2021-04-10 12:51:23 +0900

    Change borrow_summary to get USDT information

--- borrow_summary.py
+++ borrow_summary.py
@@ -1,38 +1,40 @@
 import os
 from datetime import datetime
-
-from influxdb import InfluxDBClient
+from logging import getLogger, StreamHandler, Formatter, INFO
 
 from ftx import Ftx
+from influxdb import InfluxDBClient
+
+
+coins = ['USD', 'USDT']
+
+logger = getLogger(__name__)
+logger.setLevel(INFO)
+logger.propagate = False
+h = StreamHandler()
+h.setLevel(INFO)
+h.setFormatter(Formatter('%(levelname)s %(asctime)s %(filename)s %(message)s'))
+logger.addHandler(h)
+
+
+def conv(t, coin, size):
+    return {'measurement': 'borrow_summary',
+            'time': t,
+            'tags': {'coin': coin},
+            'fields': {'size': size}}
 
 
 def main():
-
-    ftx = Ftx()
-    result = ftx.spot_margin_borrow_summary()
+    logger.info("Start getting the borrow summary.")
+    result = Ftx().spot_margin_borrow_summary()
     now = datetime.utcnow()
-    points = []
-    for r in result:
-        if r['coin'] == 'USD':
-            p = {
-                'measurement': 'borrow_summary',
-                'time': now,
-                'tags': {
-                    'coin': r['coin']
-                },
-                'fields': {
-                    'size': r['size']
-                }
-            }
-            points.append(p)
-            print(p)
+    points = [conv(now, r['coin'], r['size']) for r in result if r['coin'] in coins]
+    idb = InfluxDBClient(os.environ['DB_HOST'], os.environ['DB_PORT'],
+            os.getenv('DB_USER'), os.getenv('DB_PASS'), os.environ['DB_NAME'])
+    #idb.write_points(points)
+    logger.info(f"IDB client have written {len(points)} records.")
+    logger.info("End.")
 
-    idb = InfluxDBClient(host=os.environ['DB_HOST'],
-                         port=os.environ['DB_PORT'],
-                         username=os.getenv('DB_USER', ''),
-                         password=os.getenv('DB_PASS', ''),
-                         database=os.environ['DB_NAME'])
-    idb.write_points(points)
 
 if __name__ == '__main__':
     main()
